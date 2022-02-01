97704b49b650871698b3e129dc8e871df2c9a0ae
Cmaan-R <robsonleandrox@hotmail.com>
2022-02-01 14:32:59 -0300

    a

---
+++ exercicios/src/fundamentos/PrimitivoVSObjeto.java
@@ -0,0 +1,19 @@
+package fundamentos;
+
+public class PrimitivoVSObjeto {
+	
+	public static void main(String[] args) {
+		
+		String s = new String ("texto");
+		s.toUpperCase();
+		
+		System.out.println(s);
+		
+		//Wrappers são a versão objeto dos tipos primitivos!!!
+		int a = 123;
+		System.out.println(a);
+		
+		
+	
+	}
+}
