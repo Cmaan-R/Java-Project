fc0f033f7e9719a4da5240e2627823705c19c8c3
Cmaan-R <robsonleandrox@hotmail.com>
2022-02-03 07:35:21 -0300

    update

---
+++ exercicios/src/fundamentos/DesafioAritmedicos.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+import org.graalvm.compiler.core.match.MatchableNode;
+
+public class DesafioAritmedicos {
+
+		public static void main(String[] args) {
+			
+			//int a = (int) Math.pow(b, 2);
+			
+			
+			//System.out.println(a);
+			
+		}
+}
