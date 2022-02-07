91341c18bd52936e26685ad7cae3b791c31d0eb4
Cmaan-R <robsonleandrox@hotmail.com>
2022-02-07 12:05:11 -0300

    update novo modulo

---
+++ exercicios/src/estruturasDeControle/If.java
@@ -0,0 +1,10 @@
+package estruturasDeControle;
+
+public class If {
+	
+		public static void main(String[] args) {
+			
+			System.out.println("Ola, Mundo");
+		}
+
+}
--- exercicios/src/fundamentos/DesafioCalculadora.java
+++ exercicios/src/fundamentos/DesafioCalculadora.java
@@ -1,7 +1,36 @@
 package fundamentos;
 
+import java.util.Scanner;
+
 public class DesafioCalculadora {
-	
-	//testegithub
+
+	public static void main(String[] args) {
+				
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Digite o primeiro valor: ");
+		Double num1 = entrada.nextDouble();
+		
+		System.out.println("Digite a operação desejada: ");
+		String op = entrada.next();
+		
+		System.out.println("Digite o segundo valor: ");
+		Double num2 = entrada.nextDouble();
+		
+		
+		//Lógica
+		double resultado = "+".equals(op) ? num1 + num2 : 0;
+		resultado = "-".equals(op)? num1 - num2 : resultado ;
+		resultado = "*".equals(op)? num1 * num2 : resultado;
+		resultado = "/".equals(op)? num1 / num2 : resultado;
+		 
+		
+		System.out.printf("%.2f %s %.2f = %.2f" , num1 , op, num2 , resultado);
+		
+		entrada.close();		
+		
+		
+	}
 
 }
+  
