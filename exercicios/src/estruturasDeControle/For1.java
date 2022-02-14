ad3233aaba71681e7589ac0cdf4294ef6e853baf
Cmaan-R <robsonleandrox@hotmail.com>
2022-02-14 13:34:38 -0300

    update

---
+++ exercicios/src/estruturasDeControle/For1.java
@@ -0,0 +1,29 @@
+package estruturasDeControle;
+
+public class For1 {
+	
+		public static void main(String[] args) {
+			
+			/*int contador = 0 ;
+			
+			
+			while(contador <= 10) {
+				System.out.printf("\n i = %d", contador);
+				contador += 2;*/
+		//	}
+			
+			for (int contador = 1; contador <= 10;contador ++) {
+					System.out.printf("\n i = %d", contador);
+					
+			
+			}
+			
+			int x= 2;
+			for (; x < 10; ) {
+				System.out.println("x " + x);
+				x++;
+			}
+
+	}
+
+}
---
+++ exercicios/src/estruturasDeControle/WhileDeterminado.java
@@ -0,0 +1,19 @@
+package estruturasDeControle;
+
+public class WhileDeterminado {
+	
+		public static void main(String[] args) {
+			
+			int contador = 0;
+			
+			
+			while(contador <= 10) {
+				System.out.printf("\n i = %d", contador);
+				contador += 2;
+			
+
+			
+		}
+	}
+}
+
