0bcd6993fa293776631488669ea69ba91b729a62
Cmaan-R <robsonleandrox@hotmail.com>
2022-02-17 14:11:12 -0300

    Update Quarta Feira 17/02

---
+++ exercicios/src/classesMetodos/PrimeiroDesafio.java
@@ -0,0 +1,28 @@
+  package classesMetodos;
+
+public class PrimeiroDesafio {
+	
+	int valor = 3; // Não pode mexer nessa linha
+	static int b = 4; // Este não precisa de uma Instância para ser acessado
+	
+	
+	public static void main(String[] args) {
+		
+		//Pode mexer apenas aqui
+		PrimeiroDesafio p = new PrimeiroDesafio();
+		
+		
+		System.out.println(p.valor);
+		
+		System.out.println(b);
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+
+}
