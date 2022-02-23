4391e2f897730232452d7fec0362725cec8b4ebe
Cmaan-R <robsonleandrox@hotmail.com>
2022-02-23 12:45:40 -0300

    Update Quarta Feira 23/02

--- exercicios/src/arrays/Matriz.java
+++ exercicios/src/arrays/Matriz.java
@@ -1,5 +1,6 @@
 package arrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Matriz {
@@ -28,6 +29,12 @@ public class Matriz {
 		
 		double media = total / (qtdeAlunos * qtdeNotas);
 		System.out.println("Média da turma é " + media );
+		
+		for(double [] notasDoAluno : notasDaTurma) {
+			
+			System.out.println(Arrays.toString(notasDoAluno));
+		}
+		
 		
 		entrada.close();
 		
