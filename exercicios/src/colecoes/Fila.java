6c9d9e68ae85487d03d0b3b46915424baffa09ac
E-134 <E-134@DESKTOP-HC13ERC>
2022-03-06 18:36:36 -0300

    Update Domingo 06/03

---
+++ exercicios/src/colecoes/Fila.java
@@ -0,0 +1,50 @@
+package colecoes;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Fila {
+	
+	public static void main(String[] args) {
+		
+		Queue<String> fila = new LinkedList<>();
+		
+		// Offer e Add -> Adicionam elementos na fila
+		// Diferença é o comportamento ocorre quando a fila está cheia
+		
+		fila.add("Ana"); // Retorna False
+		fila.offer("Bia");  //Lança uma excessão
+		fila.add("Carlos");
+		fila.offer("Daniel");
+		fila.add("Rafaela");
+		fila.offer("Gui");
+		
+		//Peek e Element -> obter o próximo elemento da fila (sem remover)
+		//Diferença é o comportamento ocorre quando a lista está vazia 
+		System.out.println(fila.peek());  //Retorna Null
+		System.out.println(fila.peek());
+		System.out.println(fila.element()); // Lança uma exceção
+		System.out.println(fila.element());
+		
+
+		
+		//Poll e Remove -> obter o próximo elemento da fila e remove!
+		System.out.println(fila.poll());
+		System.out.println(fila.remove());
+		System.out.println(fila.poll());
+		System.out.println(fila.poll());
+		System.out.println(fila.poll());
+		System.out.println(fila.poll());
+		System.out.println(fila.poll());
+		System.out.println(fila.poll());	// Retorna False
+		System.out.println(fila.remove());  // Lança uma excessão
+		 
+		//fila.size();
+		//fila.clear();
+		//fila.isEmpty();
+		//fila.contains(...);
+		
+		
+	}
+
+}
