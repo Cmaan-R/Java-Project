b4d869899608dfe16a0d6042d85231cd7ce38e01
Compaq <Compaq@DESKTOP-1SE9FLK>
2022-03-12 18:58:35 -0300

    Update Sabado 12/03

---
+++ exercicios/src/colecoes/Hash.java
@@ -0,0 +1,23 @@
+package colecoes;
+
+import java.util.HashSet;
+
+public class Hash {
+	
+	public static void main(String[] args) {
+		
+		HashSet<Usuario> usuarios = new HashSet<Usuario>();
+		
+		usuarios.add(new Usuario("Pedro"));
+		usuarios.add(new Usuario("Rebecca"));
+		usuarios.add(new Usuario("Dalia"));
+		
+		boolean resultado = usuarios.contains(new Usuario ("Dalia")); 
+		System.out.println(resultado);
+		
+		
+		
+		
+	}
+
+}
