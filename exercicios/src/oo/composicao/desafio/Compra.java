6460ec6aaa92c16778cb7835b114c28832625ea9
E-134 <E-134@DESKTOP-HC13ERC>
2022-03-31 18:08:23 -0300

    Update

--- exercicios/src/oo/composicao/desafio/Compra.java
+++ exercicios/src/oo/composicao/desafio/Compra.java
@@ -1,5 +1,29 @@
 package oo.composicao.desafio;
 
-public class Compra {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Compra {
+	
+	final List<Item> itens = new ArrayList<>();
+	
+	void adicionarItem (Produto p , int quantidade) {
+		this.itens.add(new Item (p , quantidade));
+				
+	}
+	
+	void adicionarItem (String nome , double preco, int quantidade) {
+		this.itens.add(new Item (new Produto (nome ,preco), quantidade));
+				
+	}
+
+	double obterValorTotal () {
+		double total = 0;
+		
+		for (Item item : itens) {
+			total += item.quantidade * item.produto.preco;
+		}
+		
+		return total;
+	}
 }
