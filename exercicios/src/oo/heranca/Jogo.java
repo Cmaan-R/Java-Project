0cc0456f8720d583440a9e55a6253d27722b28f8
E-134 <E-134@DESKTOP-HC13ERC>
2022-04-11 22:31:00 -0300

    Update

--- exercicios/src/oo/heranca/Jogo.java
+++ exercicios/src/oo/heranca/Jogo.java
@@ -4,28 +4,35 @@ public class Jogo {
 	
 	public static void main(String[] args) {
 		
-		Monstro j1 = new Monstro ();
-		j1.x = 10;
-		j1.y = 10;
+		Monstro monstro = new Monstro ();
+		monstro.x = 10;
+		monstro.y = 10;
 		
-		Heroi j2 = new Heroi ();
-		j2.x = 10;
-		j2.y = 11;
+		Heroi heroi = new Heroi ();
+		heroi.x = 10;
+		heroi.y = 11;
 		
 		
 		
-		System.out.println(j1.vida);
-		System.out.println(j2.vida);
+		System.out.println(monstro.vida);
+		System.out.println(heroi.vida);
 		
-		j1.atacar(j2);
 		
-		System.out.println(j1.vida);
-		System.out.println(j2.vida);
+		System.out.println("Monstro tem " + monstro.vida);
+		System.out.println("Heroi tem " + heroi.vida);
 		
-		j2.atacar(j1);
+		heroi.atacar(monstro);
+		monstro.atacar(heroi);
 		
-		System.out.println(j1.vida);
-		System.out.println(j2.vida);
+		heroi.atacar(monstro);
+		monstro.atacar(heroi);
+		
+		monstro.andar(Direcao.NORTE);
+		heroi.atacar(monstro);
+		monstro.atacar(heroi);
+		
+		System.out.println("Heroi tem " + heroi.vida);
+		System.out.println("Monstro tem " + monstro.vida);
 		
 		
 		
