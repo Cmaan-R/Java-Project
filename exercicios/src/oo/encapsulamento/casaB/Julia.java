f73369b4b7c021adea6bbf4c5eea101bb2d20557
Cmaan-R <robsonleandrox@hotmail.com>
2022-04-13 15:50:58 -0300

    Update

---
+++ exercicios/src/oo/encapsulamento/casaB/Julia.java
@@ -0,0 +1,22 @@
+package oo.encapsulamento.casaB;
+
+import oo.encapsulamento.casaA.Ana;
+
+public class Julia {
+	
+	void testeAcessos () {
+		
+		Ana sogra = new Ana();
+		
+		//segredo
+		//facoDentroDeCasa
+		//formaDeFalar
+		//todosSabem
+		
+		System.out.println(sogra.todosSabem);
+		
+		
+		
+	}
+
+}
