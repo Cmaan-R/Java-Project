b302875be6048a922d3827ee9091b21f0155dac8
Cmaan-R <robsonleandrox@hotmail.com>
2022-04-14 08:01:51 -0300

    Update

--- exercicios/src/oo/encapsulamento/PessoaTeste.java
+++ exercicios/src/oo/encapsulamento/PessoaTeste.java
@@ -6,9 +6,9 @@ public class PessoaTeste {
 		
 	
 	Pessoa p1 = new Pessoa();
-	p1.idade = -30;
+	p1.idade = -30; //Alterar o valor da variável
 	
-	System.out.println(p1.idade);
+	System.out.println(p1.idade);	//Lendo o valor da variável
 
 	}
 
