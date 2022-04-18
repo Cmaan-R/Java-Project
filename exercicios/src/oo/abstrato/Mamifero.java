73f63f50fd2e171402d37c1dd386f605da86159f
Cmaan-R <robsonleandrox@hotmail.com>
2022-04-18 11:55:26 -0300

    Update

--- exercicios/src/oo/abstrato/Mamifero.java
+++ exercicios/src/oo/abstrato/Mamifero.java
@@ -2,11 +2,7 @@ package oo.abstrato;
 
 public abstract class Mamifero extends Animal {
 	
-	@Override
-	public String mover() {
-		return "Usando as patas";
-	}
-	
+		
 	public abstract String mamar();
 	
 }
