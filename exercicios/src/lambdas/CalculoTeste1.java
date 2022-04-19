9c4324fb8db9000e6b36c694b0a3e41375a50dca
Cmaan-R <robsonleandrox@hotmail.com>
2022-04-19 15:42:52 -0300

    Update

--- exercicios/src/lambdas/CalculoTeste1.java
+++ exercicios/src/lambdas/CalculoTeste1.java
@@ -4,10 +4,11 @@ public class CalculoTeste1 {
 
 	public static void main(String[] args) {
 		
-		Calculo soma = new Soma ();
-		Calculo multiplicacao = new Multiplicar ();
+		Calculo calculo = new Somar ();
+		System.out.println(calculo.executar(2, 3));
 		
-		System.out.println(soma.executar(2, 3));
+		calculo = new Multiplicar ();
+		System.out.println(calculo.executar(2, 3 ));
 	}
 	
 }
