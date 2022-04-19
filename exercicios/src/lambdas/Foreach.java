989e12f69e1984f26f4cf2457260e73bfe068b2c
Cmaan-R <robsonleandrox@hotmail.com>
2022-04-19 18:46:42 -0300

    Update

--- exercicios/src/lambdas/Foreach.java
+++ exercicios/src/lambdas/Foreach.java
@@ -18,9 +18,24 @@ public class Foreach {
 		System.out.println("\nLAMBDA NA VERSÃO 1");
 		aprovados.forEach(nome -> System.out.println(nome + "!!!"));
 		
-		System.out.println("\nMethod Reference...");
+		System.out.println("\nMethod Reference 1 ...");
 		aprovados.forEach(System.out::println);
+		
+		System.out.println("\nLAMBDA NA VERSÃO 2");
+		aprovados.forEach(nome -> meuImprimir(nome + " !"));
+		
+		System.out.println("\nMethod Reference 2 ...");
+		aprovados.forEach(Foreach:: meuImprimir);
 		
 	}
 
+	static void meuImprimir (String nome) {
+		System.out.println("Oi! Meu nome é: " + nome);
+	}
+	
+	
+	
+	
+	
+		
 }
