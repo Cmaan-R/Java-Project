e358d4d5f9275c4cb1faf900fe5062cb11ea441d
Cmaan-R <robsonleandrox@hotmail.com>
2022-04-19 15:11:49 -0300

    Update

--- exercicios/src/oo/heranca/teste/CarroTeste.java
+++ exercicios/src/oo/heranca/teste/CarroTeste.java
@@ -42,8 +42,6 @@ public class CarroTeste {
 	
 	System.out.println("A Velocidade Atual da " + bmw.nome + " é de " + bmw.velocidadeAt +"km");
 	System.out.println("A Velocidade Atual do " + civic.nome + " é de " + civic.velocidadeAt +"km \n");
-	
-	//Teste de Github
 
 	}
 }
