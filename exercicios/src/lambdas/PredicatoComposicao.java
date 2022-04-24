b8c86d1a885d6d4f467e7ab3a2a12c686a2de7a7
E-134 <E-134@DESKTOP-HC13ERC>
2022-04-24 15:31:43 -0300

    Update

---
+++ exercicios/src/lambdas/PredicatoComposicao.java
@@ -0,0 +1,20 @@
+package lambdas;
+
+import java.util.function.Predicate;
+
+public class PredicatoComposicao {
+	
+	public static void main(String[] args) {
+		
+		Predicate<Integer> isPar =
+				numero -> numero %2 == 0;		
+		
+		Predicate<Integer> isTresDigitos = 
+				num -> num >= 100 && num <=999;
+				
+		System.out.println(isPar.test(23));
+		System.out.println(isTresDigitos.test(212));
+				
+	}
+
+}
