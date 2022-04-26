5e157e3fea2173c9089d8e537c880f3f7e3dbd6e
Cmaan-R <robsonleandrox@hotmail.com>
2022-04-26 14:46:37 -0300

    Update

--- exercicios/src/streams/DesafioMap.java
+++ exercicios/src/streams/DesafioMap.java
@@ -3,6 +3,8 @@ package streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public class DesafioMap {
 	
@@ -10,10 +12,21 @@ public class DesafioMap {
 				
 		List<Integer> nums = Arrays.asList( 1 , 2 , 3, 4, 5, 6 , 7, 8,9);
 
+		UnaryOperator<String> inverter = i -> new StringBuilder(i).reverse().toString();
+		
+		Function<String, Integer> reverter = r ->  Integer.parseInt(r);
+		
+		
 		
 		nums.stream()
 		.map(Integer::toBinaryString)
-		.forEach(System.out::println);		 
+		.map(inverter)
+		.map(reverter)
+		.forEach(System.out::println);
+		
+		
+		
+		
 		
 		
 		
