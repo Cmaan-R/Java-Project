f100e254f18615178a82d1f9b165076f9c43388f
Cmaan-R <robsonleandrox@hotmail.com>
2022-04-26 12:10:09 -0300

    Update

--- exercicios/src/streams/DesafioMap.java
+++ exercicios/src/streams/DesafioMap.java
@@ -1,13 +1,28 @@
 package streams;
 
+
 import java.util.Arrays;
 import java.util.List;
 
 public class DesafioMap {
 	
 	public static void main(String[] args) {
-		
+				
 		List<Integer> nums = Arrays.asList( 1 , 2 , 3, 4, 5, 6 , 7, 8,9);
+
+		
+		nums.stream()
+		.map(Integer::toBinaryString)
+		.forEach(System.out::println);		 
+		
+		
+		
+		
+		
+			
+		//String stream = Integer.toBinaryString(nums);
+		
+		
 		
 //		1. Número para string binária... 6=> "110";
 //		2. Inverter a String... "110" => "011";
