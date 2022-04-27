03305b153caaa2ba03e5bfc77301bc5c10844a50
Cmaan-R <robsonleandrox@hotmail.com>
2022-04-27 08:22:23 -0300

    Update

---
+++ exercicios/src/streams/DesafioFilter.java
@@ -0,0 +1,33 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class DesafioFilter {
+	
+	public static void main(String[] args) {
+		
+		Trabalhador t1 = new Trabalhador ("Pedro" , false, "Cliente" );
+		Trabalhador t2 = new Trabalhador ("Patrícia", true , "Excelente");
+		Trabalhador t3 = new Trabalhador ("Vidal" ,  true, "Médio");
+		Trabalhador t4 = new Trabalhador ("Daniel" ,  true, "Muito Bom");
+		
+		List<Trabalhador> trabalhadores = Arrays.asList(t1 , t2 , t3 , t4);
+		Predicate<Trabalhador> fazParte = t -> t.fazParte;
+		
+		
+		Function<Trabalhador, String> bemVindo =
+				t -> "Bem vindo " 
+						+ t.nome
+						+ " Desejamos Um Bom dia de trabalho\n"
+						+ "Nível de Conceito = "
+						+ t.conceito + "\n";
+		
+		trabalhadores.stream()
+		.filter(fazParte)
+		.map(bemVindo)
+		.forEach(System.out::println);
+	}
+}
---
+++ exercicios/src/streams/Trabalhador.java
@@ -0,0 +1,15 @@
+package streams;
+
+public class Trabalhador {
+	
+	final String nome;
+	final boolean fazParte;
+	final String conceito;
+	
+	public Trabalhador (String nome, boolean fazParte , String conceito) {
+		this.nome = nome;
+		this.fazParte = fazParte;
+		this.conceito = conceito;
+	}
+
+}
