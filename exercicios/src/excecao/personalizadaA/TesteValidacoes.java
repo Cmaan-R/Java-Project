848b2f3ec0c0ae75cdd6ae9efd4fcbd4d73258c6
Cmaan-R <robsonleandrox@hotmail.com>
2022-05-03 14:27:36 -0300

    Update

--- exercicios/src/excecao/personalizadaA/TesteValidacoes.java
+++ exercicios/src/excecao/personalizadaA/TesteValidacoes.java
@@ -7,11 +7,13 @@ public class TesteValidacoes {
 	public static void main(String[] args) {
 		
 		try {
-			Aluno aluno = new Aluno ("Ana" , -7);
+			Aluno aluno = new Aluno ("Ana" , 7);
 			Validar.aluno(aluno);
+			
+			Validar.aluno(null);
 		} catch (StringVaziaException e) {
 			System.out.println(e.getMessage());
-		} catch (NumeroForaIntervaloException a) {
+		} catch (NumeroForaIntervaloException | IllegalArgumentException e) {
 			System.out.println("O Número está fora do intervalo");
 		}
 		
