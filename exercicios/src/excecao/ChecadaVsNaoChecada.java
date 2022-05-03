f2c454f1e10131953296c3720fb6d9e370904fd2
Cmaan-R <robsonleandrox@hotmail.com>
2022-05-03 08:27:55 -0300

    Update

---
+++ exercicios/src/excecao/ChecadaVsNaoChecada.java
@@ -0,0 +1,35 @@
+package excecao;
+
+public class ChecadaVsNaoChecada {
+	
+	public static void main(String[] args)  {
+		try {
+			geraErro1();
+		} catch (RuntimeException e) {
+			System.out.println(e.getMessage());;
+		}
+		
+		try {
+			geraErro2();			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		System.out.println("Fim :D	 !!!");
+		
+	}
+
+	// Exceção Não Checada ou Não Verificada
+	static void geraErro1 () throws RuntimeException {
+		throw new RuntimeException("Ocorreu um erro bem legal #01!");
+	}
+	
+	
+	// Exceção Checada ou Verificada
+	static void geraErro2 () throws Exception{
+		
+		throw new RuntimeException("Ocorreu um erro bem legal #02!");
+		
+	}
+	
+}
